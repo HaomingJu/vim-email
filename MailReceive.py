5634e0f33ca7c80d161a74fe2468d0cb8dcc5787
juhaoming@outlook.com <juhaoming@trunk.tech>
2021-09-10 19:33:52 +0800

    Parse the title data of inbox/mails (解析收件箱中邮件标题)

--- MailReceive.py
+++ MailReceive.py
@@ -2,7 +2,46 @@
 
 import imaplib
 import email
+from email.header import Header, decode_header, make_header
+import re
 import logging
+
+# 保存邮箱地址, 为了后面的通讯录建设
+class MailAddress:
+    def __init__(self, desc_, addrs_):
+        self.Description = desc_
+        self.Address = addrs_
+
+
+# 邮件标头, 用于速看
+class MailTitle:
+    def __init__(self, title):
+        # sele.mailReg = "@"
+        self.Subject = make_header(decode_header(title['Subject']))
+        self.Date = make_header(decode_header(title['Date']))
+        self.From = []
+        self.To = []
+        self.Attach = None
+        self.UID = -1
+
+        # Tos = str(make_header(decode_header(title['To']))).split(',')
+        # for ele in Tos:
+            # ele = ele.strip()
+            # ret = re.search('(?P<description>.+)\s+<(?P<address>.+@.+)>', ele)
+            # if ret is None:
+                # ret = re.search('(?P<address>.+@.+)', ele)
+                # logging.info(ret.groupdict()['address'])
+            # else:
+                # logging.info(ret.groupdict()['description'])
+                # logging.info(ret.groupdict()['address'])
+
+        Froms = str(make_header(decode_header(title['From'])))
+        logging.info(Froms)
+
+
+        # logging.info(self.Subject)
+        # logging.info(make_header(decode_header(title['To'])))
+
 
 
 
@@ -15,6 +54,7 @@ class MailReceive:
         self.imap_user_name = user_name
         self.imap_user_pwd = user_pwd
         self.client = imaplib.IMAP4_SSL(self.imap_ssl_addr, self.imap_ssl_port)
+        self.Mails = []
         try:
             self.client.login(self.imap_user_name, self.imap_user_pwd)
             logging.info("Login successful.")
@@ -24,6 +64,13 @@ class MailReceive:
 
         self.mailFolders = {}
 
+    def getMailNums(self):
+        return self.Mails.__len__()
+
+    # Close link with remote while destroy this object (析构时关闭同远程服务器的链接)
+    def __del__(self):
+        self.client.logout()
+
     # Get all folder's name of remote mail server (获取远程服务器中所有文件夹名称)
     def fetchFolder(self):
         ret, mailFolders = self.client.list()
@@ -31,6 +78,19 @@ class MailReceive:
             folderName = ele.decode('utf-8').split(' "/" ')[-1]
             self.mailFolders[folderName] = []
             logging.info(folderName)
+        # TODO: 正确显示中文
+
+    def selectInboxAll(self):
+        self.client.select(readonly=True)
+        ret, inboxUnseen = self.client.search(None, 'ALL')
+        inboxUnseen = inboxUnseen[0].split()
+
+        for ele in inboxUnseen:
+            mailRet, mailData = self.client.fetch(str(ele.decode('utf-8')), '(UID BODY[HEADER])')
+            if mailData[0] is None and mailRet == 'OK':
+                continue
+            mailMessage = email.message_from_bytes(mailData[0][1])
+            self.Mails.append(MailTitle(mailMessage))
 
 
 if __name__ == '__main__':
@@ -38,25 +98,9 @@ if __name__ == '__main__':
 
     gIMAP_SSL_ADDRESS = "imap.exmail.qq.com"
     gIMAP_SSL_PORT = 993
-    gUSER_NAME = "TODO"
-    gUSER_PWD = "TODO"
+    gUSER_NAME = None
+    gUSER_PWD = None
 
     rec = MailReceive(gIMAP_SSL_ADDRESS, gIMAP_SSL_PORT, gUSER_NAME, gUSER_PWD)
-    rec.fetchFolder()
-
-
-
-
-    # mailNums = int(messags[0])
-    # for index in range(mailNums):
-        # mailRet, mailData = client.fetch(str(index), '(UID BODY[HEADER])')
-        # if mailData[0] is None:
-            # print("NONE")
-            # continue
-
-        # mailMessage = email.message_from_bytes(mailData[0][1])
-        # print('------------------------------------------------------')
-        # print('Date: %s' % mailMessage['Date'])
-        # print('From: %s' % mailMessage['From'])
-        # print('To: %s' % mailMessage['To'])
-        # print('Subject: %s' % mailMessage['Subject'])
+    # rec.fetchFolder()
+    rec.selectInboxAll()
